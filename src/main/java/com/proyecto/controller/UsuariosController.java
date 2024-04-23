18848b579bb9929f955b84f19f6598c452605817
kevin <kevin@Kevin>
2024-04-22 23:15:23 -0600

    Correcion de funcionalidad para los botenes de editar usuarios

--- src/main/java/com/proyecto/controller/UsuariosController.java
+++ src/main/java/com/proyecto/controller/UsuariosController.java
@@ -1,28 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
- */
 package com.proyecto.controller;
 
-
+import com.proyecto.domain.Usuario;
 import com.proyecto.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author Asus
- */
 @Controller
 @RequestMapping("/usuario")
 public class UsuariosController {
-    
+
     @Autowired
     private UsuarioService usuarioService;
-    
+
     @GetMapping("/listado")
     public String listado(Model model) {
         var usuarios = usuarioService.getUsuarios();
@@ -31,5 +26,43 @@ public class UsuariosController {
         return "/usuario/listado";
     }
 
+    @GetMapping("/nuevo")
+    public String usuarioNuevo(Model model) {
+        model.addAttribute("usuario", new Usuario());
+        return "/usuario/modifica";
+    }
 
+    @PostMapping("/guardar")
+    public String usuarioGuardar(@ModelAttribute Usuario usuario) {
+        boolean nuevo = usuario.getIdUsuario() == null;
+        
+        if (nuevo) {
+            usuario.setPassword(new BCryptPasswordEncoder().encode(usuario.getPassword()));
+            usuario.setActivo(true); // Para crearlo siempre activo
+        } else {
+            Usuario actual = usuarioService.getUsuario(usuario);
+            usuario.setPassword(actual.getPassword());
+            usuario.setUsername(actual.getUsername());
+            usuario.setRoles(actual.getRoles());
+            usuario.setActivo(actual.isActivo());
+        }
+        
+        usuarioService.save(usuario, nuevo);
+        return "redirect:/usuario/listado";
+    }
+
+    @GetMapping("/eliminar/{idUsuario}")
+    public String usuarioEliminar(@ModelAttribute Usuario usuario) {
+        usuarioService.delete(usuario);
+        return "redirect:/usuario/listado";
+    }
+
+    @GetMapping("/modificar/{idUsuario}")
+    public String usuarioModificar(@ModelAttribute Usuario usuario, Model model) {
+        usuario = usuarioService.getUsuario(usuario);
+        model.addAttribute("usuario", usuario);
+        return "/usuario/modifica";
+    }
 }
+
+
